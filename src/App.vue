1d14a9a311fdf6348dc6685775160ca5206de4ca
scurenza <fedechiar@hotmail.it>
2023-02-13 15:46:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+      restaurants: []
+    }
+  },
+  created() {
+    this.getRestaurants();
+  },
+  methods: {
+    getRestaurants() {
+      axios.get('http://127.0.0.1:8000/api/userslist').then(resp => {
+        this.restaurants = resp.data.results;
+        console.log(this.restaurants);
+      })
+    }
+
+  }
+};
+
+</script>
+
+<template>
+  <div>
+    <ul v-for="restaurant in restaurants" :key="restaurant.id">
+      <li>{{ restaurant.name }}
+        <ul>
+          <li v-for="product in restaurant.products">{{ product.name }}</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *; 
+</style>
